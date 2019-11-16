dbe2761430f046bfbaa71305ba05fb6d65a942ea
arsalan.ahmed <arsalanabc@gmail.com>
2019-11-16 14:54:21 -0500

    Add welcome page and login form

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-<?php echo "welcome to my mini project"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<div>
+	<h3>welcome to my mini project </h3> 
+	<p>Please login below </p>
+</div>
+
+<div id="login">
+	<form method="post" action="">
+		<label> Username</label>
+		<input type="text" name="username">
+		<br>
+		<br>
+		<label> Password</label>
+		<input type="Password" name="password">
+		<br>
+		<br>
+		<input type="submit" name="submit">
+	</form>
+</div>
+
+</body>
+</html>
