c2873796a72c7f6fd2743726bafabe6c61ef0bc9
arsalan.ahmed <arsalanabc@gmail.com>
2019-11-16 17:40:18 -0500

    Create a file for env var

---
+++ ENV.php
@@ -0,0 +1,8 @@
+<?php
+$ROOT=pathinfo($_SERVER['SCRIPT_FILENAME']);
+
+define ('APP_PATH', '/start_mini_project');
+define ('BASE_PATH', dirname(__FILE__));
+define ('SITE_URL',    'http://'.$_SERVER['HTTP_HOST'].APP_PATH);
+
+
