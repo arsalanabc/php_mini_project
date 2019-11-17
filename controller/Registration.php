32a5622055ed5a41128a0464d046f8d93c343203
arsalan.ahmed <arsalanabc@gmail.com>
2019-11-17 00:09:55 -0500

    Create a sign up data validation

--- controller/Registration.php
+++ controller/Registration.php
@@ -18,5 +18,28 @@ class Registration {
             }
         }
     }
+
+    public function signup ($data){
+	    $data['error'] = array() ;
+	    if(self::is_empty($data['first_name'])){array_push($data["error"], "First Name cannot be empty");}
+	    if(self::is_empty($data['last_name'])) {array_push($data["error"], "Last Name cannot be empty");}
+        if(self::is_empty($data['username'])) { array_push($data["error"],"Username cannot be empty");}
+        if(self::is_empty($data['email'])) { array_push($data["error"],"Email cannot be empty");}
+        if(self::is_empty($data['password'])) {array_push($data["error"],"Password cannot be empty");}
+        if(self::is_empty($data['password_confirm'])) {array_push($data["error"], "Password Confirm cannot be empty");}
+
+        if(!self::validate_passwords($data['password'], $data['password_confirm'])) {array_push($data["error"], "Passwords do not match");}
+
+        if(sizeof($data['error']) > 0){return $data;}
+
+    }
+
+    static function is_empty($field ) {
+        return $field == "";
+	}
+
+	static function validate_passwords ($pass1, $pass2){
+	    return $pass1 == $pass2;
+    }
 }
 ?>
--- view/registration/signup.php
+++ view/registration/signup.php
@@ -3,6 +3,16 @@ require_once '../../ENV.php';
 require(BASE_PATH."/controller/Registration.php");
 require_once(BASE_PATH."/config/db_config.php");
 
+if(isset($_POST['submit'])){
+    $reg = new Registration($conn);
+    $response = $reg->signup($_POST);
+    if($response['error']){
+        foreach ($response['error'] as $error){
+            if($error != null){echo "<p style='color: red'>*".$error."</p>";}
+        }
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,29 +27,29 @@ require_once(BASE_PATH."/config/db_config.php");
 </div>
 
 <div id="login">
-    <form method="post" action="">
+    <form method="post" name="signup_form" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
         <label> First Name</label>
-        <input type="text" name="first_name" required>
+        <input type="text" name="first_name" value="<?php echo $_POST['first_name'];?>" required>
         <br>
         <br>
         <label> Last Name</label>
-        <input type="text" name="last_name" required>
+        <input type="text" name="last_name" value="<?php echo $_POST['last_name'];?>" required>
         <br>
         <br>
         <label> Username</label>
-        <input type="text" name="username" required>
+        <input type="text" name="username" value="<?php echo $_POST['username'];?>" required>
         <br>
         <br>
         <label> Password</label>
         <input type="Password" name="password" required>
         <br>
         <br>
-        <label> Email</label>
-        <input type="email" name="email" required>
-        <br>
-        <br>
         <label> Confirm Password</label>
         <input type="Password" name="password_confirm" required>
+        <br>
+        <br>
+        <label> Email</label>
+        <input type="email" name="email" value="<?php echo $_POST['email'];?>" required>
         <br>
         <br>
         <input type="submit" name="submit">
