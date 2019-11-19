11486b0fa6e570293dc1e877c0210fe596895d21
arsalan.ahmed <arsalanabc@gmail.com>
2019-11-18 19:23:19 -0500

    Clean up

--- ENV.php
+++ ENV.php
@@ -1,8 +1,7 @@
 <?php
-$ROOT=pathinfo($_SERVER['SCRIPT_FILENAME']);
 
 define ('APP_PATH', '/start_mini_project');
 define ('BASE_PATH', dirname(__FILE__));
-define ('SITE_URL',    'http://'.$_SERVER['HTTP_HOST'].APP_PATH);
+define ('SITE_URL', 'http://'.$_SERVER['HTTP_HOST'].APP_PATH);
 
 
